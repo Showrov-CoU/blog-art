1599b4891c29edd66230e41284e7f783b1596f26
Md.Ashikur Rahman (Showrov) <showrov.cse.cou@gmail.com>
2024-03-18 02:19:53 +0600

    added pagination

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -1,15 +1,18 @@
 import PropTypes from "prop-types";
+import "./blog.css";
 
 const Blog = ({ item }) => {
   return (
-    <div className="card card-compact bg-base-100 shadow-xl">
-      <figure>
+    <div className="card card-compact">
+      <figure className="w-52 h-52 mx-auto">
         <img className="size-52" src={item.img1} alt="" />
       </figure>
-      <div className="card-body">
-        <h2 className="card-title">{item.name?.substring(0, 20) + "..."}</h2>
-        <p>{item.shortDesc?.substring(0, 50) + "..."}</p>
-        <p className="font-bold">{item.author}</p>
+      <div className="card-body w-52 p-0">
+        <h2 className="card-title ps-2">
+          {item.name?.substring(0, 20) + "..."}
+        </h2>
+        <p className="ps-2">{item.shortDesc?.substring(0, 45) + "..."}</p>
+        <p className="font-bold ps-2">{item.author}</p>
       </div>
     </div>
   );
--- src/Components/ReadNext.jsx
+++ src/Components/ReadNext.jsx
@@ -1,33 +1,65 @@
 import { useEffect, useState } from "react";
 import Blog from "./Blog";
 
+import { FaAngleLeft } from "react-icons/fa";
+import { FaAngleRight } from "react-icons/fa";
+
 const ReadNext = () => {
   const [blogs, setBlogs] = useState([]);
-  //   const [currentPage, setCurrentPage] = useState(1);
-  //   const [totalPage, setTotalPage] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
   const itemPerPage = 4;
   const totalPage = Math.ceil(6 / itemPerPage);
-  //console.log(totalPage);
   const pages = [...Array(totalPage).keys()];
 
   useEffect(() => {
-    fetch("http://localhost:5000/blog")
+    fetch(`http://localhost:5000/blog?page=${currentPage}&size=${itemPerPage}`)
       .then((res) => res.json())
       .then((result) => setBlogs(result));
-  }, []);
+  }, [currentPage]);
+
+  const handlePrev = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+  const handleNext = () => {
+    if (currentPage < pages.length) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
   return (
-    <div>
-      <div className="flex justify-center items-center gap-5">
+    <div className="mx-40">
+      <p className="font-bold text-2xl py-5 px-5">Read Next</p>
+      <div className="flex justify-start items-center gap-5">
         {blogs.map((item) => (
           <Blog key={item._id} item={item}></Blog>
         ))}
       </div>
-      <div>
+      <div className="py-10 w-fit mx-auto flex gap-2">
+        <button
+          onClick={handlePrev}
+          className="btn btn-outline btn-primary border-0 flex justify-center items-center"
+        >
+          <FaAngleLeft></FaAngleLeft>Previous
+        </button>
         {pages.map((page) => (
-          <button key={page} className="btn">
+          <button
+            onClick={() => setCurrentPage(page + 1)}
+            key={page}
+            className={`btn btn-primary ${
+              currentPage === page + 1 ? "btn-active" : "btn-outline"
+            }`}
+          >
             {page + 1}
           </button>
         ))}
+        <button
+          onClick={handleNext}
+          className="btn btn-outline btn-primary border-0 flex justify-center items-center"
+        >
+          Next <FaAngleRight></FaAngleRight>
+        </button>
       </div>
     </div>
   );
